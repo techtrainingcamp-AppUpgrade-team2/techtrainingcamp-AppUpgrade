b44808ab7ab2dc871442ee2fdcfc6747f1977e4d
G-ghy <1095322940@qq.com>
2021-11-10 21:49:22 +0800

    添加注释

--- model/deviceIDCache.go
+++ model/deviceIDCache.go
@@ -4,6 +4,7 @@ import (
 	"sync"
 )
 
+// DeviceIDCache	DeviceID缓存对象，定义了规则对应的设备白名单信息
 type DeviceIDCache struct {
 	DeviceIDCache map[RuleIdType]map[DeviceIdType]struct{}
 }
@@ -11,6 +12,10 @@ type DeviceIDCache struct {
 var instance *DeviceIDCache
 var once sync.Once
 
+// @title             GetInstance
+// @description       返回DeviceID缓存对象类的唯一实例
+// @auth              高宏宇         2021/11/10
+// @return            *instance     DeviceIDCache
 func GetInstance() DeviceIDCache {
 	once.Do(func() {
 		instance = new(DeviceIDCache)
